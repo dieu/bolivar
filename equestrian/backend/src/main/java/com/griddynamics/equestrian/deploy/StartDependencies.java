9f1367715be0fe81bd705861a467818bfd924a4c
unknown <apanasenko@.main.mirantis.sar>
2009-04-30 13:21:16 +0400

    create equestrian application.

---
+++ equestrian/backend/src/main/java/com/griddynamics/equestrian/deploy/StartDependencies.java
@@ -0,0 +1,11 @@
+package com.griddynamics.equestrian.deploy;
+
+/**
+ * @author: apanasenko aka dieu
+ * Date: 29.04.2009
+ * Time: 19:41:30
+ */
+public interface StartDependencies {
+	void deploy();
+	boolean verify();
+}
---
+++ equestrian/backend/src/main/java/com/griddynamics/equestrian/deploy/impl/StartVirtualMachinesImpl.java
@@ -0,0 +1,18 @@
+package com.griddynamics.equestrian.deploy.impl;
+
+import com.griddynamics.equestrian.deploy.StartVirtualMachines;
+
+/**
+ * @author: apanasenko aka dieu
+ * Date: 29.04.2009
+ * Time: 19:48:35
+ */
+public class StartVirtualMachinesImpl implements StartVirtualMachines {
+	public void create() {
+		//To change body of implemented methods use File | Settings | File Templates.
+	}
+
+	public boolean verify() {
+		return true;
+	}
+}
