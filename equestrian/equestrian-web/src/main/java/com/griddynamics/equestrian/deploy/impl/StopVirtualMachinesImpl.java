246aca515c1cfca70d82b3a56f5bef852e6f714e
Dieu <apanasenko@griddynamics.com>
2009-06-17 13:50:10 +0400

    add button Shutdown Cloud in UI.

--- equestrian/equestrian-web/src/main/java/com/griddynamics/equestrian/deploy/impl/StopVirtualMachinesImpl.java
+++ equestrian/equestrian-web/src/main/java/com/griddynamics/equestrian/deploy/impl/StopVirtualMachinesImpl.java
@@ -1,6 +1,15 @@
 package com.griddynamics.equestrian.deploy.impl;
 
 import com.griddynamics.equestrian.deploy.StopVirtualMachines;
+import com.griddynamics.equestrian.helpers.AmazonKeys;
+import com.xerox.amazonws.ec2.Jec2;
+import com.xerox.amazonws.ec2.ReservationDescription;
+import com.xerox.amazonws.ec2.EC2Exception;
+import com.xerox.amazonws.ec2.TerminatingInstanceDescription;
+import com.xerox.amazonws.typica.jaxb.TerminateInstances;
+
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  * @author: apanasenko aka dieu
@@ -8,11 +17,67 @@ import com.griddynamics.equestrian.deploy.StopVirtualMachines;
  * Time: 14:25:55
  */
 public class StopVirtualMachinesImpl implements StopVirtualMachines{
-	public void stop() {
-		//To change body of implemented methods use File | Settings | File Templates.
-	}
+    private AmazonKeys aws;
 
-	public boolean verify() {
-		return true;
-	}
+    public StopVirtualMachinesImpl() {
+    }
+
+    public void setAws(AmazonKeys aws) {
+        this.aws = aws;
+    }
+
+    public void stop() {
+
+        try {
+            Jec2 ec2 = new Jec2(aws.getAWSAccessKeyId(), aws.getSecretAccessKey());
+            List<String> terminateInstances = monitoringInstance(ec2);
+            List<TerminatingInstanceDescription> terminateInstancesList = ec2.terminateInstances(terminateInstances);
+            while(true) {
+                terminateInstances = monitoringInstance(ec2);
+                if(terminateInstances.size() == 0) {
+                    return;
+                } else {
+                    Thread.sleep(30000L);
+                }
+            }
+        } catch (EC2Exception e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean verify() {
+        return true;
+    }
+
+    private List<String> monitoringInstance(Jec2 ec2) throws EC2Exception {
+        List<String> terminateInstances = new ArrayList<String>();
+        List<String> params = new ArrayList<String>();
+        List<ReservationDescription> instances = ec2.describeInstances(params);
+        for (ReservationDescription res : instances) {
+            if (res.getInstances() != null) {
+                for (ReservationDescription.Instance inst : res.getInstances()) {
+                    if(inst.isRunning() && (aws.getUserId().equals("") || inst.getKeyName().equals(aws.getUserId()))) {
+                        if(inst.getImageId().equals(aws.getWorkerImageId())) {
+                            if(inst.isRunning()) {
+                                terminateInstances.add(inst.getInstanceId());
+                            }
+                        }
+                        if(inst.getImageId().equals(aws.getServerImageId())) {
+                            if(inst.isRunning()) {
+                                terminateInstances.add(inst.getInstanceId());
+                            }
+                        }
+                        if(inst.getImageId().equals(aws.getSchedulerImageId())) {
+                            if(inst.isRunning()) {
+                                terminateInstances.add(inst.getInstanceId());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return terminateInstances;
+    }
 }
